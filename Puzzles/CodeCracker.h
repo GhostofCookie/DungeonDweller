45f4149663f1eb0cc257bc6f050f6a8b5aa21fa2
siwt2720 <tyler.siwy@uleth.ca>
2017-10-25 17:16:21 -0600

    regetting the makefile

---
+++ Puzzles/CodeCracker.h
@@ -0,0 +1,66 @@
+//
+// code_cracker.h
+//
+/// \author Tyler Siwy
+/// \date Oct 20, 2017
+//
+
+#ifndef  CODE_CRACKER_H
+#define CODE_CRACKER_H
+#include "Puzzle.h"
+
+///This class contains the mini-game/puzzle Code Cracker
+class CodeCracker: virtual Puzzle
+{
+  public:
+   ///Default constructor for hanoi, reads in riddles from .txt files
+   CodeCracker();
+
+   ///Deconstructor
+   ~CodeCracker();
+
+   ///Method to run the game, serves as a 'main' for the mini-game, calling
+   ///functions from private until the player has won.
+   void RunGame();
+  private:
+   //ALL HANDLED BY THE MENU OBJECT
+   //void PromptUser(&screen);
+   //char UserInput(Menu &menu);
+   //bool ValidCommand(char input);
+   
+   ///Checks the semantics of the user choice to make sure they aren't doing
+   ///something that would break the game with their input.
+   ///\param[in]input, has been checked for syntax by input method  
+   bool ValidMove(char Input);
+
+   ///Checks to see if the user input is one of the accepted answers.
+   ///\param[in] Input, an answer to the riddle in the form of the char.
+   bool ValidAnswer(const char Input);
+   
+   ///Checks if the player has successfully answered 3 riddles.
+   void WinCheck();
+
+   ///Checks if the player is now dead
+   void DeathCheck();//Will need to take some kind of character variable/object
+
+   /// Displays the screen containing the gameboard
+   /// \param[in] CfScreen, the screen object used for displaying the mini-game 
+   void OutputGame(Screen &ccScreen);
+
+   ///Imports riddles from a text file and stores them in the vector.
+   void ImportRiddles();
+
+   ///Vector to store the question strings read in from the file 
+   std::vector<string>>question(3);
+
+   ///Vector to store the questions answers read in from the file, in order
+   std::vector<int>>Answer(3);
+
+   ///Vector to store the descriptions on how to answer each question, in order.
+   std::vector<string>>format(3);//Descriptions of how to answer questions
+}
+#endif
+
+
+
+
---
+++ Puzzles/TicTacToe.cpp
@@ -0,0 +1,61 @@
+//
+// TicTacToe.cpp
+//
+/// \author Tyler Siwy
+/// \date Oct 20, 2017
+//  
+#include "TicTacToe.h"
+
+TicTacToe::TicTacToe(){
+
+}
+
+TicTacToe::~TicTacToe()
+{
+
+}
+
+void TicTacToe::RunGame(){
+
+}
+
+// void TicTacToe::PromptUser(&screen)
+// {
+
+// }
+
+//char TicTacToe::UserInput(Menu &menu)
+//{
+//  return 'c';
+//}
+
+//bool TicTacToe::ValidMove(char input)
+//{
+//   return true;
+//}
+
+
+//bool TicTacToe::ValidCommand(char input)
+//{
+//   return true;
+//}
+
+// void TicTacToe::OutputGame(&screen)
+// {
+
+// }
+
+void TicTacToe::MovePiece(int InputX, int InputY)
+{
+
+}
+
+void TicTacToe::WinCheck()
+{
+
+}
+
+void TicTacToe::AiMove()
+{
+
+}
