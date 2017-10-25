5513b14b6cf5389c2f216888b69e21a330e0f5e3
buer2720 <rylan.bueckert@uleth.ca>
2017-10-25 16:33:20 -0600

    Documentation and completion of RoomTree and Item and subclasses

--- Item/Consumable.h
+++ Item/Consumable.h
@@ -1,8 +1,16 @@
+//
+// Consumable Header File
+//
+/// \author Rylan Bueckert
+/// \date Oct 25, 2017
+
 #ifndef CONSUMABLE_H
 #define CONSUMABLE_H
 #include "Item.h"
 using namespace std;
 
+/// This class is an abstaract base class
+/// derived from Item to represent in game consumables
 class Consumable : public Item
 {};
 #endif
