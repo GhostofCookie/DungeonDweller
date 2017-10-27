88e7ab0d67a4c70f08b5bd46227764a14d0db564
rigt2720 <t.rigaux@uleth.ca>
2017-10-27 11:54:41 -0600

    Forgot to add these as well yesterday. My bad.

---
+++ Menu/FightMenu.cpp
@@ -0,0 +1,17 @@
+#include "FightMenu.h"
+
+FightMenu::FightMenu()
+{
+}
+
+FightMenu::~FightMenu()
+{
+}
+
+void FightMenu::SetOptions(map<string> OptionsList, string type)
+{
+}
+
+void FightMenu::HandleInput(istream & is)
+{
+}
---
+++ Menu/MainMenu.cpp
@@ -0,0 +1,17 @@
+#include "MainMenu.h"
+
+MainMenu::MainMenu()
+{
+}
+
+MainMenu::~MainMenu()
+{
+}
+
+void MainMenu::SetOptions(map<string> OptionsList, string type)
+{
+}
+
+void MainMenu::HandleInput(istream & is)
+{
+}
