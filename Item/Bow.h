124aed78b1d6a1aa526617fb7ee8e8415dc454d6
buer2720 <rylan.bueckert@uleth.ca>
2017-10-26 19:27:54 -0600

    removed old files from Item/

--- Item/Bow.h
+++
@@ -1,9 +0,0 @@
-#ifnedef BOW_H
-#define BOW_H
-#include "Weapon.h"
-
-class Bow : public Weapon
-{
-   void use(Character *target);
-};
-#endif
--- Item/Spell.h
+++
@@ -1,9 +0,0 @@
-#ifnedef SPELL_H
-#define SPELL_H
-#include "Weapon.h"
-
-class Spell : public Weapon
-{
-   void use(Character *target)
-};
-#endif
