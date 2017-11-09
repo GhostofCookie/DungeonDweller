6829b4a5ce5a4966626fd785ad28f7348c7dc66d
rigt2720 <t.rigaux@uleth.ca>
2017-11-09 00:37:00 -0700

    Is it working yet? Or will it vanish into the ether once again...

--- Menu/menuTest.cpp
+++ Menu/menuTest.cpp
@@ -1,56 +1,40 @@
-#include <iostream>
-#include "MainMenu.h"
-#include "ExploreMenu.h"
-#include "FightMenu.h"
-#include "TradeMenu.h"
-#include "DialogueMenu.h"
-#include "RiddleMenu.h"
-#include "TicTacToeMenu.h"
-#include "HanoiMenu.h"
-#include "CharacterMenu.h"
-#include "MemoryMenu.h"
-#include "ConnectFourMenu.h"
-using namespace std;
+/// \author Tomas Rigaux
+/// \date 8/11/2017
+///
+/// This is the testing class for Menu. 
+///
 
-int main()
+#include "MenuTest.h"
+
+/// Sets up the test variables for testing.
+void MenuTest::setUp()
 {
-  Menu *menu= new Menu(10,101);
-  map<int,string> options;
-  options.insert(pair<int,string>(1,"Option 1"));
-  options.insert(pair<int,string>(2,"Option 2"));
-  options.insert(pair<int,string>(3,"Option 3"));
-  options.insert(pair<int,string>(4,"Option 4"));
-  // Testing all menu outputs //
-  menu->SetOptions(options);
-  menu->OutputMenu();
-  menu = new MainMenu();
-  menu->SetOptions(options);
-  menu->OutputMenu();
-  menu->HandleInput(cin);
-  /*  menu = new ExploreMenu();
-  menu->SetOptions(options);
-  menu->OutputMenu();
-  menu = new FightMenu();
-  menu->SetOptions(options);
-  menu->OutputMenu();
-  menu = new TradeMenu();
-  menu->SetOptions(options);
-  menu->OutputMenu();
-  menu = new DialogueMenu();
-  menu->SetOptions(options);
-  menu->OutputMenu();
-  menu = new TicTacToeMenu();
-  menu->SetOptions(options);
-  menu->OutputMenu();
-  menu = new ConnectFourMenu();
-  menu->SetOptions(options);
-  menu->OutputMenu();
-  menu = new RiddleMenu();
-  menu->SetOptions(options);
-  menu->OutputMenu();
-  menu= new HanoiMenu();
-  menu->SetOptions(options);
-  menu->OutputMenu();
-  */
-  return 0;
+	menu = new Menu();
+	menu2 = new Menu(100, 100);
+	options.insert(pair<int, string>(1, "I'm the first option!"));
+	options.insert(pair<int, string>(2, "I'm the second!"));
+}
+/// Deletes any pointers to prevent memory leak.
+void MenuTest::tearDown()
+{
+	//   delete menu;
+}
+/// Tests that the constructor is wokring in several varieties of construction.
+void MenuTest::testConstructors()
+{
+
+}
+/// Tests the setting of options.
+void MenuTest::testSetting()
+{
+	menu->SetOptions(options);
+	menu2->SetOptions(options, 5, 10);
+}
+/// Tests that menu builds correctly.
+void MenuTest::testBuilding()
+{
+	menu->SetOptions(options);
+	menu->OutputMenu();
+	menu2->SetOptions(options, 10, 20);
+	menu2->OutputMenu();
 }
