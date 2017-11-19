29f54f68e77abad165a0c9deb378c0e58b3ac895
paur2720 <reid.paulhus@uleth.ca>
2017-11-19 16:29:41 -0700

    changed main setup

--- Game/DungeonDweller.cpp
+++ Game/DungeonDweller.cpp
@@ -5,57 +5,58 @@
 /// This is the main file for the game.
 
 // Screen Headers
-#include "Screen/Screen.h"
-#include "Screen/SlotScreen.h"
+#include "../Screen/Screen.h"
+#include "../Screen/SlotScreen.h"
 // Image Headers
-#include "Image/Image.h"
-#include "Image/DefaultImg.h"
-#include "Image/ImportImg.h"
+#include "../Image/Image.h"
+#include "../Image/DefaultImg.h"
+#include "../Image/ImportImg.h"
 // Room Header
-#include "Room/Room.h"
+#include "../Room/Room.h"
 // RoomTree Headers
-#include "RoomTree/RoomTree.h"
+#include "../RoomTree/RoomTree.h"
 // Menu Headers
-#include "Menu/CharacterMenu.h"
-#include "Menu/ConnectFourMenu.h"
-#include "Menu/DialogueMenu.h"
-#include "Menu/ExploreMenu.h"
-#include "Menu/FightMenu.h"
-#include "Menu/GameMenu.h"
-#include "Menu/HanoiMenu.h"
-#include "Menu/MainMenu.h"
-#include "Menu/MemoryMenu.h"
-#include "Menu/Menu.h"
-#include "Menu/RiddleMenu.h"
-#include "Menu/TicTacToeMenu.h"
-#include "Menu/TradeMenu.h"
+#include "../Menu/CharacterMenu.h"
+#include "../Menu/ConnectFourMenu.h"
+#include "../Menu/DialogueMenu.h"
+#include "../Menu/ExploreMenu.h"
+#include "../Menu/FightMenu.h"
+#include "../Menu/GameMenu.h"
+#include "../Menu/HanoiMenu.h"
+#include "../Menu/MainMenu.h"
+#include "../Menu/MemoryMenu.h"
+#include "../Menu/Menu.h"
+#include "../Menu/RiddleMenu.h"
+#include "../Menu/TicTacToeMenu.h"
+#include "../Menu/TradeMenu.h"
 // GameState Headers
-#include "GameState/DialogueState.h"
-#include "GameState/FightState.h"
-#include "GameState/MainState.h"
-#include "GameState/PuzzleState.h"
-#include "GameState/ExploreState.h"
-#include "GameState/GameState.h"
-#include "GameState/MainState.h"
-#include "GameState/TradeState.h"
+#include "../GameState/DialogueState.h"
+#include "../GameState/FightState.h"
+#include "../GameState/MainState.h"
+#include "../GameState/PuzzleState.h"
+#include "../GameState/ExploreState.h"
+#include "../GameState/GameState.h"
+#include "../GameState/MainState.h"
+#include "../GameState/TradeState.h"
 // Puzzle Headers
-#include "Puzzles/CodeCracker.h"
-#include "Puzzles/ConnectFour.h"
-#include "Puzzles/Hanoi.h"
-#include "Puzzles/MemoryMatch.h"
-#include "Puzzles/Puzzle.h"
-#include "Puzzles/TicTacToe.h"
+#include "../Puzzles/CodeCracker.h"
+#include "../Puzzles/ConnectFour.h"
+#include "../Puzzles/Hanoi.h"
+#include "../Puzzles/MemoryMatch.h"
+#include "../Puzzles/Puzzle.h"
+#include "../Puzzles/TicTacToe.h"
 // Item Headers
-#include "Item/MyConsumables.h"
-#include "Item/MyWeapons.h"
+#include "../Item/MyConsumables.h"
+#include "../Item/MyWeapons.h"
 // Character Headers
-#include "Character/Npc.h"
-#include "Character/Player.h"
+#include "../Character/Npc.h"
+#include "../Character/Player.h"
+using namespace std;
 
 
 
 int main()
 {
-   
+   cout << "main worked" << endl;
    return 0;
 }
