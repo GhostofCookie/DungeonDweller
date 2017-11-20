30c72c531d9d107fc84a7f4be5b7f86f5b71b821
buer2720 <rylan.bueckert@uleth.ca>
2017-11-20 15:22:35 -0700

    oriention Tranlator moved to main

--- Game/DungeonDweller.cpp
+++ Game/DungeonDweller.cpp
@@ -55,9 +55,20 @@
 // Misc Headers
 #include <iostream>
 #include <ctime> //srand()
+#include <cctype>
+#include <stdexcept>
 using namespace std;
 
+//***FUNCTION PROTOTYPES********************************************************
 
+// Orientation Translation Functions
+void FindNewOrientation(char[4] directions, char dir, int treeHeight);
+void ShiftDir(char[4] directions,unsigned int numOfShifts);
+char Orientation(char[4] directions, char dir);
+
+
+
+//******************************************************************************
 
 int main()
 {
@@ -78,6 +89,15 @@ int main()
   Room *roomptr = new Room(imageImport.collection);
   RoomTree roomTree(roomptr);
 
+  // Remember Orientation
+  /*
+    Whenever roomTree.Move(dir) is used and is successfull, it must be used like this:
+
+    roomTree.Move(Orientation(directions),dir);
+    FindNewOrientation(directions,dir,roomTree.CurrentHeight());
+    
+   */
+  char directions[4] = {'p','l','c','r'};
    
   char n;
   while(true)
@@ -128,3 +148,64 @@ int main()
   }
   return 0;
 }
+
+
+//***FUNCTION DEFINITIONS*******************************************************
+
+// Orientation Translation Functions
+void FindNewOrientation(char[4] directions, char dir, int treeHeight)
+{
+   dir = toupper(dir);
+   switch(dir) {
+      case 'P':
+	 if(treeHeight == 1)
+	    ShiftDir(directions,2);
+	 break;
+      case 'L':
+	 ShiftDir(directions,1);
+	 break;
+      case 'R':
+	 ShiftDir(directions,3);
+	 break;
+      case 'C':
+	 break;
+      default:
+	 throw invalid_argument("invalid direction");
+   }
+}
+
+void ShiftDir(char[4] directions,unsigned int numOfShifts)
+{
+   for(int i = 0; i < numOfShifts; ++i) {
+      char temp = directions[0];
+      directions[0] = directions[1];
+      directions[1] = directions[2];
+      directions[2] = directions[3];
+      directions[3] = temp;
+   }
+}
+
+char Orientation(char[4] directions, char dir)
+{
+   switch(dir) {
+      case 'P':
+	 return directions[0];
+	 break;
+      case 'L':
+	 return directions[1];
+	 break;
+      case 'R':
+	 return directions[3];
+	 break;
+      case 'C':
+	 return directions[2];
+	 break;
+      default:
+	 throw invalid_argument("invalid direction");
+   }
+
+}
+
+
+
+//******************************************************************************
