3dd430998801bd2154abf77cc78acb29e80d0b9b
siwt2720 <tyler.siwy@uleth.ca>
2017-11-22 22:12:01 -0700

    Trying to get Hanoi to output properly, logic is working

--- Puzzles/Hanoi.h
+++ Puzzles/Hanoi.h
@@ -68,10 +68,10 @@ class Hanoi: public virtual Puzzle
 
    void BoardSetup();
 
-   void DrawDiscOnTargetStack(int targetSize);
+   void DrawDiscOnTargetStack(int targetSize, int sourceStack);
    
    /// The vector which holds each towers' defaultImg
-   std::vector<stack<DefaultImg*>>tower;
+   std::vector<stack<DefaultImg>>tower;
    
    /// Number of stacks in the game
    ///X-coordinate size in the tower vector
@@ -89,10 +89,7 @@ class Hanoi: public virtual Puzzle
    /// GameMenu is used for handling input
 
    HanoiMenu HanoiGameMenu;
-   ///A vector to store the size of all the discs in the game, stored in
-   ///ascending order of size of discs. 0=3,1=7,2=11,3=15
-   std::vector<DefaultImg*>discsVector;
-
+   
    string menuOption1, menuOption2, menuOption3,
       menuOption4, menuOption5, menuOption6;
 };
