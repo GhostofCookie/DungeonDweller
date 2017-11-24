b8d464587fdd3da01ac956e11991295c3522c0a9
rigt2720 <t.rigaux@uleth.ca>
2017-11-23 21:38:38 -0700

    Added randomization to puzzles.

--- GameState/PuzzleState.cpp
+++ GameState/PuzzleState.cpp
@@ -9,6 +9,9 @@ void PuzzleState::Set()
 {
   puzzles.push_back(new Hanoi());
   puzzles.push_back(new TicTacToe());
+  puzzles.push_back(new ConnectFour());
+  puzzles.push_back(new MemoryMatch());
+  puzzles.push_back(new CodeCracker());
 }
 
 void PuzzleState::Get()
@@ -16,5 +19,5 @@ void PuzzleState::Get()
   random_shuffle(puzzles.begin(), puzzles.end());
   auto it = puzzles.begin();
   
-  it->RunGame();
+  (*it)->RunGame();
 }
