f1444a8eeb85c66969a5bf41d82ac13945785efc
paur2720 <reid.paulhus@uleth.ca>
2017-11-23 23:32:42 -0700

    added room test program

--- Room/main.cpp
+++ Room/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "Room.h"
 #include "../Screen/Screen.h"
+#include "../Room/Room.h"
+#include "../ImageImporter/ImageImporter.h"
 #include "../Image/Image.h"
 #include "../Image/DefaultImg.h"
 #include "../Image/ImportImg.h"
@@ -9,6 +11,17 @@ using namespace std;
 
 int main()
 {
+   Screen screen = Screen();
+   ImageImporter import("../DD_Art/DD_MasterFileLinux.txt");
+   Room room = Room(import.collection);
 
+
+   cout << room.GetType() << endl;
+   room.AlignCenter(screen);
+   room.Draw(screen);
+   cout << screen << endl;
+
+
+   
    return 0;
 }
