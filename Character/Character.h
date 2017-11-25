96288e4ad6aeae41e6d520bc7942b05bd5dd8303
Gates (Gates) Kempenaar <kemg2720@36widow.cs.uleth.ca>
2017-11-24 18:42:37 -0700

    new functionalities of character

--- Character/Character.h
+++ Character/Character.h
@@ -10,22 +10,25 @@
 
 #include <stdio.h>
 #include <iostream>
-#include "../Item/Item.h"
+#include "../Item/MyConsumables.h"
 #include "../Screen/Screen.h"
-#include "../Image/Image.h"
+#include "../Image/ImportImg.h"
+#include <vector>
 
 
 using namespace std;
+class Item;
 
 ///Abstract base class of Npc's and Player's attributes
 class Character
 {
-  private:
+   
+  protected:
+   
    ImportImg img;
    
-protected:
    ///Vector of items the character has
-   vector<*Item> inventory;
+   vector<Item> inventory;
 
    ///Amount of gold player has
    int gold;
@@ -33,40 +36,37 @@ protected:
    ///Characters health
    int health;
    
-  public:
+   public:
    /// Character constructor
    /// \param[in] g, starting amoutn of gold for the character
    /// \param[in] h, starting health for the character
-   Character(int g = 0, int h = 0, ImportImg img = ImportImg());
+   //virtual Character() = 0;
 
    /// Character deconstructor
-   ~Character();
+   virtual ~Character(){}
 
    ///Function to fill the characters inventory
    /// \param[in] inventory, the vector that stores the items in inventory
-   virtual void FillInventory(vector<*Item> &inventory, item item);
+   virtual void FillInventory(const Item &item);
    
    ///Shows a list of inventory items
    /// \param[in] Inventory, a pointer to a vector of inventory items
-   virtual Item GetInventoryItems(Item *inventory);
+   virtual void GetInventoryItems(const Item &inventory);
 
    ///Chooses the item to be used
    /// \param[in] Inventory, a pointer to a vector of inventory items
-   virtual Item UseItem(Item *inventory);
+   virtual void UseItem(const Item &inventory);
 
    ///Change the amount of gold a character has
    /// \param[in&] Gold, current amount of gold the character has
    /// \param[in] GoldMod, how much the current gold will be changed by
-   virtual int ChangeGold(int &gold, int goldMod);
+   virtual int ChangeGold(int goldMod);
 
    ///Changes the characters health
    /// \param[in&] H, characters current health
    /// \param[in] HMod, how the current health will be modified
-   virtual void ChangeHealth(int& h, int hMod);
+   virtual void ChangeHealth(int hMod);
 
-  
-
-  
-};
+}; 
 
 #endif /* Character_hpp */
