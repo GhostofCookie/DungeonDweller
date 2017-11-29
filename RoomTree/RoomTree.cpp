54408428ae585bf58889ee1623561d34af8e0cdd
buer2720 <rylan.bueckert@uleth.ca>
2017-11-29 14:39:28 -0700

    Fixed memory leaks and undid Tyler's medling

--- RoomTree/RoomTree.cpp
+++ RoomTree/RoomTree.cpp
@@ -33,10 +33,6 @@ RoomTree::RoomTree(Room* rootRoom)
 RoomTree::~RoomTree()
 {
    DeleteTree(root);
-   delete root;
-   delete currNode;
-   root = nullptr;
-   currNode = nullptr;
 }
 
 /// Helper function to assist in deleting the tree
@@ -57,6 +53,7 @@ void RoomTree::DeleteTree(Node *tempRoot)
       if('D' != tempRoot->RootDir) //delete down child if its not the parent node
 	 DeleteTree(tempRoot->down);
 
+      delete tempRoot->room; //delete the room
       delete tempRoot; //delete this node
    }
 }
@@ -206,30 +203,35 @@ unsigned int RoomTree::NodesInBranch(Node *tempRoot) const
    {
       switch(tempRoot->RootDir)
       {
+	 case '\0':
+	    return NodesInBranch(tempRoot->left)
+	       + NodesInBranch(tempRoot->down)
+	       + NodesInBranch(tempRoot->right)
+	       + NodesInBranch(tempRoot->up) + 1;
 	 case 'U':
 	    return NodesInBranch(tempRoot->left)
 	       + NodesInBranch(tempRoot->down)
-	       + NodesInBranch(tempRoot->right);
+	       + NodesInBranch(tempRoot->right) + 1;
 	    break;
 	 case 'L':
 	    return NodesInBranch(tempRoot->up)
 	       + NodesInBranch(tempRoot->down)
-	       + NodesInBranch(tempRoot->right);
+	       + NodesInBranch(tempRoot->right) + 1;
 	    break;
 	 case 'R':
 	    return NodesInBranch(tempRoot->left)
 	       + NodesInBranch(tempRoot->down)
-	       + NodesInBranch(tempRoot->up);
+	       + NodesInBranch(tempRoot->up) + 1;
 	    break;
 	 case 'D':
 	    return NodesInBranch(tempRoot->left)
 	       + NodesInBranch(tempRoot->up)
-	       + NodesInBranch(tempRoot->right);
+	       + NodesInBranch(tempRoot->right) + 1;
 	    break;
 	 default:
 	    break;
       }
    }
 
-   return 1;
+   return 0;
 }
--- RoomTree/RoomTreeTest.cpp
+++ RoomTree/RoomTreeTest.cpp
@@ -6,43 +6,43 @@
 
 void RoomTreeTest::setUp()
 {
-	Tree1 = new RoomTree(nullptr);
-	Tree2 = new RoomTree(nullptr);
-	Tree3 = new RoomTree(nullptr);
-	Tree1->NewRoom('l',nullptr);
-	Tree1->NewRoom('d',nullptr);
-	Tree1->NewRoom('r',nullptr);
-	Tree1->NewRoom('u',nullptr);
-	Tree3->NewRoom('D',nullptr);
-	Tree3->Move('D');
-	Tree3->NewRoom('D',nullptr);
-	Tree3->Move('D');
-	Tree3->NewRoom('D',nullptr);
-	Tree3->Move('D');
-	Tree3->NewRoom('D',nullptr);
-	Tree3->Move('D');
-	Tree3->NewRoom('D',nullptr);
-	Tree3->Move('D');
+   Tree1 = new RoomTree(nullptr);
+   Tree2 = new RoomTree(nullptr);
+   Tree3 = new RoomTree(nullptr);
+   Tree1->NewRoom('l',nullptr);
+   Tree1->NewRoom('d',nullptr);
+   Tree1->NewRoom('r',nullptr);
+   Tree1->NewRoom('u',nullptr);
+   Tree3->NewRoom('D',nullptr);
+   Tree3->Move('D');
+   Tree3->NewRoom('D',nullptr);
+   Tree3->Move('D');
+   Tree3->NewRoom('D',nullptr);
+   Tree3->Move('D');
+   Tree3->NewRoom('D',nullptr);
+   Tree3->Move('D');
+   Tree3->NewRoom('D',nullptr);
+   Tree3->Move('D');
 }
 
 void RoomTreeTest::tearDown()
 {
-	delete Tree1;
-	delete Tree2;
-	delete Tree3;
+   delete Tree1;
+   delete Tree2;
+   delete Tree3;
 }
 
 void RoomTreeTest::testMove()
 {
-	CPPUNIT_ASSERT(Tree1->Move('l'));
-	CPPUNIT_ASSERT(!Tree1->Move('l'));
-	CPPUNIT_ASSERT(!Tree1->Move('d'));
-	CPPUNIT_ASSERT(!Tree1->Move('u'));
-	CPPUNIT_ASSERT(Tree1->Move('r'));
-	CPPUNIT_ASSERT(Tree1->Move('r'));
-	CPPUNIT_ASSERT(!Tree1->Move('r'));
-	CPPUNIT_ASSERT(Tree1->Move('l'));
-	CPPUNIT_ASSERT_THROW(Tree1->Move('x'),invalid_argument);
+   CPPUNIT_ASSERT(Tree1->Move('l'));
+   CPPUNIT_ASSERT(!Tree1->Move('l'));
+   CPPUNIT_ASSERT(!Tree1->Move('d'));
+   CPPUNIT_ASSERT(!Tree1->Move('u'));
+   CPPUNIT_ASSERT(Tree1->Move('r'));
+   CPPUNIT_ASSERT(Tree1->Move('r'));
+   CPPUNIT_ASSERT(!Tree1->Move('r'));
+   CPPUNIT_ASSERT(Tree1->Move('l'));
+   CPPUNIT_ASSERT_THROW(Tree1->Move('x'),invalid_argument);
 }
 
 void RoomTreeTest::testNewRoom()
@@ -54,18 +54,18 @@ void RoomTreeTest::testNewRoom()
 
 void RoomTreeTest::testAt()
 {
-	CPPUNIT_ASSERT(Tree1->At() == nullptr);
+   CPPUNIT_ASSERT(Tree1->At() == nullptr);
 }
 
 void RoomTreeTest::testCurrentHeight()
 {
-	CPPUNIT_ASSERT(Tree1->CurrentHeight() == 1);
-	CPPUNIT_ASSERT(Tree2->CurrentHeight() == 1);
-	CPPUNIT_ASSERT(Tree3->CurrentHeight() == 6);
+   CPPUNIT_ASSERT(Tree1->CurrentHeight() == 1);
+   CPPUNIT_ASSERT(Tree2->CurrentHeight() == 1);
+   CPPUNIT_ASSERT(Tree3->CurrentHeight() == 6);
 }
 
 void RoomTreeTest::testTotalNodes()
 {
-	CPPUNIT_ASSERT(Tree1->TotalNodes() == 5);
-	CPPUNIT_ASSERT(Tree3->TotalNodes() == 6);
+   CPPUNIT_ASSERT(Tree1->TotalNodes() == 5);
+   CPPUNIT_ASSERT(Tree3->TotalNodes() == 6);
 }
